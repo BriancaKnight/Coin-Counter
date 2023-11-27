f182e04b4410e392be3c48a6d86ef2b9aa739a79
Brianca Knight <briancaknight@gmail.com>
2023-11-27 13:58:48 -0800

    Add change prototype to calculate coin amounts.
    Co-authored-by: Monica Barboza<MonBoza@gmail.com>

--- src/js/project.js
+++ src/js/project.js
@@ -1,21 +1,29 @@
 export class CoinCounter {
-    constructor(amount) {
-        this.amount = amount;
+  constructor(amount) {
+    this.amount = amount;
+    this.coins = { quarters: 0, dimes: 0, nickles: 0, pennies: 0 };
+  }
+
+  change() {
+    if (this.amount <= 0) {
+      return 0;
+    }
+    if (this.amount >= 0.25) {
+      this.coins.quarters++;
+      this.amount -= 0.25;
+    } else if (this.amount >= 0.10) {
+      this.coins.dimes++;
+      this.amount -= 0.10;
+    } else if (this.amount >= 0.05) {
+      this.coins.nickles++;
+      this.amount -= 0.05;
+    } else if (this.amount >= 0.01) {
+      this.coins.pennies++;
+      this.amount -= 0.01;
+    } else {
+      return this.coins
     }
 
-    quarter() {
-      return Math.floor(this.amount / .25);
-    }
-
-    dime() {
-        return Math.floor(this.amount / .10);
-    }
-
-    nickle() {
-      return Math.floor(this.amount / .05);
-    }
-
-    penny() {
-        return this.amount / .01;
-    }
+    return this.change();
+  }
 }
